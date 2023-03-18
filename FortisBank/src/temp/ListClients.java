5e2e635de54b0fb055e9ed1a0172e0ffd4367145
dominicpotvin <39275064+dominicpotvin@users.noreply.github.com>
2023-03-18 13:39:52 -0400

    mise a jour de listClient

--- FortisBank/src/temp/ListClients.java
+++ FortisBank/src/temp/ListClients.java
@@ -1,26 +1,39 @@
 package temp;
-import bus.Client;
+
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 
 public class ListClients {
 	
-	private List<Client> Client;	
-
+	private List<Client> clients;	
 
 	public List<Client> getClient() {
-		return Client;
-	}
-
-	public void setClient(List<Client> client) {
-		Client = client;
+		return clients;
 	}
 	
 	// constructeur
-	public void listClients() {
+	public ListClients() {
+		clients = new ArrayList<>();
+	}
 		
+	
+	// methode pour ajouter une transaction
+	public void addClients(Client client) {
+		clients.add(client);
 	}
 	
+	// getter pour acceder a la liste des transaction de l'exterieur
+	public List<Client> getClients(){
+		return clients;
+	}
+	
+	/*
+	 * créer des instances de cette classe, 
+	 * ajouter des clienta à la liste et 
+	 * accéder à la liste des clients à partir d'autres parties du programme.*/	
 	
 	
 }
