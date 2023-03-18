0c8488202aed2012f462fcc752c6b40bdb0986b0
dominicpotvin <39275064+dominicpotvin@users.noreply.github.com>
2023-03-18 08:26:51 -0400

    Create tester1.java

---
+++ FortisBank/src/client/tester1.java
@@ -0,0 +1,21 @@
+package client;
+
+
+
+import bus.Customer;
+import bus.*;
+
+public class tester1 {
+
+	public static void main(String[] args) {
+		
+		
+		Customer customer1 = new Customer("CkAc001","Titi les bobette","titi@gmail.com");
+
+		
+		
+		System.out.println(customer1);
+
+	}
+
+}
